ffa2308aabd66c03cbc26711d622387eaa35a397
Tiago Wolker <tiago.wolker@ntconsult.com.br>
2019-09-22 08:15:12 -0300

    Criação do ReportBuilder para montar o relatório de saída

--- src/main/java/com/twl/salesfileprocessor/builder/ReportBuilder.java
+++ src/main/java/com/twl/salesfileprocessor/builder/ReportBuilder.java
@@ -1,6 +1,7 @@
 package com.twl.salesfileprocessor.builder;
 
 import com.twl.salesfileprocessor.model.Venda;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,7 +20,33 @@ public class ReportBuilder {
         return new ReportBuilder();
     }
 
-    public ReportBuilder withAmountClients(Long amount) {
+    /**
+     * Monta o cabeçalho do relatório com o nome do arquivo.
+     *
+     * @param fileName nome do arquivo de entrada
+     * @return {@link ReportBuilder}
+     */
+    public ReportBuilder withHeader(String fileName) {
+
+        builder.append("### Relatório do arquivo");
+
+        if (fileName != null && !fileName.trim().isEmpty()) {
+            builder.append(fileName);
+        }
+        builder.append(" ###")
+                .append(System.getProperty("line.separator"));
+
+        return this;
+    }
+
+    /**
+     * Monta a quantidade de clientes no relatório.
+     *
+     * @param amount Quantidade de clientes
+     * @return {@link ReportBuilder}
+     */
+    public ReportBuilder withQuantidadeClientes(Long amount) {
+
         if (amount > 0) {
             builder.append("Quantidade de clientes no arquivo de entrada: ")
                     .append(amount)
@@ -28,23 +55,43 @@ public class ReportBuilder {
         return this;
     }
 
-    public ReportBuilder withAmountSalesMan(Long amount) {
+    /**
+     * Monta a quantidade de vendedores no relatório.
+     *
+     * @param amount Quantidade de vendedores
+     * @return {@link ReportBuilder}
+     */
+    public ReportBuilder withQuantidadeVendedores(Long amount) {
+
         if (amount > 0) {
-            builder.append("Quantidade de vendedor no arquivo de entrada: ")
+            builder.append("Quantidade de vendedores no arquivo de entrada: ")
                     .append(amount)
                     .append(System.getProperty("line.separator"));
         }
         return this;
     }
 
-    public ReportBuilder withSalesMoreExpensive(Optional<Venda> venda) {
+    /**
+     * Monta a venda mais cara no relatório.
+     *
+     * @param venda Venda mais cara.
+     * @return {@link ReportBuilder}
+     */
+    public ReportBuilder withVendaMaisCara(Optional<Venda> venda) {
+
         venda.ifPresent(value -> builder.append("ID da venda mais cara: ")
                 .append(value.getIdVenda())
                 .append(System.getProperty("line.separator")));
         return this;
     }
 
-    public ReportBuilder withWorstSeller(Optional<List<Venda>> sales) {
+    /**
+     * Monta o pior vendedor no relatório.
+     *
+     * @param sales Vendas informadas no arquivo de entrada
+     * @return {@link ReportBuilder}
+     */
+    public ReportBuilder withPiorVendedor(Optional<List<Venda>> sales) {
         sales.ifPresent(saleList -> builder.append("O pior vendedor: ")
                 .append(saleList.iterator().next().getNomeVendedor()));
 
